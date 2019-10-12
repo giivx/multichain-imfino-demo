7cd06a4ee904945f4a5841ec9434acd10eda14c6
Bojan Nišević <bojan@pop-os.localdomain>
2019-10-12 21:09:37 +0200

    Changed code.

--- index.php
+++ index.php
@@ -111,11 +111,13 @@
 	}
 ?>
 			<footer>
-				<div class="container">
-						<img src="https://imfino.com/static/images/imfino_logo_white.png" class="footer-logo img-responsive">
-						<div class="copyright text-right">
-								© Imfino 2019
-						</div>
+				<div class="footer-copyright">
+					<div class="container">
+							<img src="https://imfino.com/static/images/imfino_logo_white.png" class="footer-logo img-responsive">
+							<div class="copyright text-right">
+									© Imfino 2019
+							</div>
+					</div>
 				</div>
 			</footer>
 		</div>
