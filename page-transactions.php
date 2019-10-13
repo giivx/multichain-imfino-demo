e2ca2623ed9abb7788643af46474dbee7891c81a
Bojan Nišević <bojan@pop-os.localdomain>
2019-10-14 01:58:26 +0200

    Code change.

--- page-transactions.php
+++ page-transactions.php
@@ -2,7 +2,6 @@
   no_displayed_error_result($transactions, multichain('listwallettransactions', 100, 0, false, true));
 ?>
 
-<table class="table table-bordered table-condensed table-break-words table-striped">
 <pre>
 <?php
   //$transactions = array_reverse($transactions)
@@ -11,4 +10,24 @@
   //foreach ($transactions as $tx)
  ?>
 </pre>
+
+<table class="table table-bordered table-condensed table-break-words table-striped">
+  <thead>
+    <th>Transaction ID</th>
+    <th>Valid</th>
+    <th>Block Hash</th>
+    <th>From</th>
+    <th>To</th>
+    <th>Amount (ICX)</th>
+  </thead>
+  <?php foreach ($transactions as $tx): ?>
+    <tr>
+      <td><?php $tx['txid']; ?></td>
+      <td><?php boolval($tx['valid']); ?></td>
+      <td><?php $tx['blockhash']; ?></td>
+      <td><?php $tx['myaddresses'][0]; ?></td>
+      <td><?php $tx['myaddresses'][1]; ?></td>
+      <td><?php $tx['vout'][0]['assets'][0]['qty']; ?></td>
+    </tr>
+  <?php endforeach; ?>
 </table>
