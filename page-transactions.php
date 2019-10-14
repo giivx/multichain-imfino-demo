4ab1cb6a990dc06b53e3581336ddbf5b13488f37
Bojan Nišević <bojan@pop-os.localdomain>
2019-10-14 02:00:43 +0200

    Code change.

--- page-transactions.php
+++ page-transactions.php
@@ -2,14 +2,9 @@
   no_displayed_error_result($transactions, multichain('listwallettransactions', 100, 0, false, true));
 ?>
 
-<pre>
-<?php
-  //$transactions = array_reverse($transactions)
-
-  print_r(array_reverse($transactions));
-  //foreach ($transactions as $tx)
- ?>
-</pre>
+<!-- <pre>
+<?php //print_r(array_reverse($transactions)); ?>
+</pre> -->
 
 <table class="table table-bordered table-condensed table-break-words table-striped">
   <thead>
@@ -22,12 +17,12 @@
   </thead>
   <?php foreach ($transactions as $tx): ?>
     <tr>
-      <td><?php $tx['txid']; ?></td>
-      <td><?php boolval($tx['valid']); ?></td>
-      <td><?php $tx['blockhash']; ?></td>
-      <td><?php $tx['myaddresses'][0]; ?></td>
-      <td><?php $tx['myaddresses'][1]; ?></td>
-      <td><?php $tx['vout'][0]['assets'][0]['qty']; ?></td>
+      <td><?php echo $tx['txid']; ?></td>
+      <td><?php echo boolval($tx['valid']); ?></td>
+      <td><?php echo $tx['blockhash']; ?></td>
+      <td><?php echo $tx['myaddresses'][0]; ?></td>
+      <td><?php echo $tx['myaddresses'][1]; ?></td>
+      <td><?php echo $tx['vout'][0]['assets'][0]['qty']; ?></td>
     </tr>
   <?php endforeach; ?>
 </table>
